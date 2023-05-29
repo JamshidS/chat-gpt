e6e941579894e97e85021e41c460062cb9cfd88a
Jamshid Sadiqi <jamshidsadiqi25@gmail.com>
2023-05-29 14:33:37 +0300

    create requestdto class

---
+++ src/main/java/com/chat/streaming/chatservice/dto/ChatRequest.java
@@ -0,0 +1,49 @@
+package com.chat.streaming.chatservice.dto;
+
+import java.util.ArrayList;
+
+public class ChatRequest {
+    private String model;
+    private List<Message> messageList;
+    private int n;
+    private double temperature;
+
+    public ChatRequest(String model, String prompt) {
+        this.model = model;
+
+        this.messages = new ArrayList<>();
+        this.messages.add(new Message("user", prompt));
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public List<Message> getMessageList() {
+        return messageList;
+    }
+
+    public void setMessageList(List<Message> messageList) {
+        this.messageList = messageList;
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public void setN(int n) {
+        this.n = n;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+}
