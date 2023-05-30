3898f5b11d2c704108758e2157ad734c8d586991
Jamshid Sadiqi <jamshidsadiqi25@gmail.com>
2023-05-30 17:20:24 +0300

    create test for chat method

--- src/test/java/com/chat/streaming/chatservice/controller/ChatControllerTest.java
+++ src/test/java/com/chat/streaming/chatservice/controller/ChatControllerTest.java
@@ -1,11 +1,15 @@
 package com.chat.streaming.chatservice.controller;
 
+import com.chat.streaming.chatservice.dto.ChatDto;
 import com.chat.streaming.chatservice.service.ChatService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -24,5 +28,18 @@ class ChatControllerTest {
 
     @Test
     void chat() {
+        //given
+        ChatDto chatDto = new ChatDto();
+        chatDto.setMessage("test");
+        String responseFromOpenAi = "something related to the test";
+
+        //when
+        Mockito.when(chatService.getChat(chatDto)).thenReturn(responseFromOpenAi);
+        ResponseEntity<Object> response = underTest.chat(chatDto);
+
+        //then
+        assertEquals(HttpStatus.OK,response.getStatusCode());
+        assertEquals(responseFromOpenAi,response.getBody());
+        Mockito.verify(chatService,Mockito.times(1)).getChat(chatDto);
     }
 }
