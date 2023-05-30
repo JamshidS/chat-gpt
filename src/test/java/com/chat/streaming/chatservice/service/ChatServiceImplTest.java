83e703c9419a413109fa4ecb443f5d4938787ac6
Jamshid Sadiqi <jamshidsadiqi25@gmail.com>
2023-05-30 17:27:20 +0300

    setup mocks

--- src/test/java/com/chat/streaming/chatservice/service/ChatServiceImplTest.java
+++ src/test/java/com/chat/streaming/chatservice/service/ChatServiceImplTest.java
@@ -1,10 +1,27 @@
 package com.chat.streaming.chatservice.service;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.client.RestTemplate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ChatServiceImplTest {
+
+    @Qualifier("openaiRestTemplate")
+    @Mock
+    private RestTemplate restTemplate;
+
+    @InjectMocks private ChatServiceImpl underTest;
+
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.openMocks(this);
+    }
 
     @Test
     void getChat() {
