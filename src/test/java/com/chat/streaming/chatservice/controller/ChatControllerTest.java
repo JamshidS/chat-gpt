3e8e21e1259bbfa80fe156abf079a76e8b6d1887
Jamshid Sadiqi <jamshidsadiqi25@gmail.com>
2023-05-30 17:08:59 +0300

    setup the mocks

--- src/test/java/com/chat/streaming/chatservice/controller/ChatControllerTest.java
+++ src/test/java/com/chat/streaming/chatservice/controller/ChatControllerTest.java
@@ -1,10 +1,26 @@
 package com.chat.streaming.chatservice.controller;
 
+import com.chat.streaming.chatservice.service.ChatService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ChatControllerTest {
+
+    @Mock
+    private ChatService chatService;
+
+    @InjectMocks
+    private ChatController underTest;
+
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.openMocks(this);
+    }
 
     @Test
     void chat() {
