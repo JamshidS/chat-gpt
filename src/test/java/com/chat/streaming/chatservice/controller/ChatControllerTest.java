b25f604eb7bddd297e52f02779d37fda7cc11f91
Jamshid Sadiqi <jamshidsadiqi25@gmail.com>
2023-05-30 17:06:59 +0300

    create test class

---
+++ src/test/java/com/chat/streaming/chatservice/controller/ChatControllerTest.java
@@ -0,0 +1,12 @@
+package com.chat.streaming.chatservice.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChatControllerTest {
+
+    @Test
+    void chat() {
+    }
+}
